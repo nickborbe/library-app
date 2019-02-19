f7563b293c62d4d84c05e79f97460c69cc74c044
nickborbe <nickborbe@gmail.com>
2019-02-19 10:26:17 -0500

    more stuff

--- models/Book.js
+++ models/Book.js
@@ -6,6 +6,7 @@ const bookSchema = new Schema({
 
     title: {type: String},
     author: {type: Schema.Types.ObjectId, ref: 'Author'},
+    // this tells mongoose that author field will be an ID and the model it will correspond to will be called 'Author'
     image: String
 
 })
