7ff2f366460b26aabd1f1a7ac587b23d02f48d1a
nickborbe <nickborbe@gmail.com>
2019-09-17 09:36:46 -0400

    author model

---
+++ models/Author.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+
+const authorSchema = new Schema({
+
+    title: String,
+    birthday: String,
+    image: String
+
+})
+
+
+
+const Author = mongoose.model('Author', authorSchema);
+//                              |
+// mongoose will create a collection called books because we called the model Book
+// it takes the word, makes it lower case, and then puts an S on the end
+
+
+
+module.exports = Author;
+
+
